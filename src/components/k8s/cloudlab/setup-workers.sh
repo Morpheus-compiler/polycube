5ffb044fc1953adeb6b026a9fc6d94f0ffc6446c
Sebastiano Miano <mianosebastiano@gmail.com>
2022-11-16 11:55:51 +0100

    Update scripts with the user

--- src/components/k8s/cloudlab/setup-workers.sh
+++ src/components/k8s/cloudlab/setup-workers.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo "No arguments supplied"
+    echo "The first argument should be the user"
+    exit 1
+fi
+
+USER_HOME=$(getent passwd $1 | awk -F: '{ print $6 }')
+
+if [ -z "${USER_HOME}" ]; then
+    echo "Unknown user $1"
+    exit 1
+fi
+
 INSTALL_K8S_SCRIPT="install-k8s-deps.sh"
 INSTALL_K8S_SCRIPT_URL="https://raw.githubusercontent.com/Morpheus-compiler/polycube/morpheus-k8s/scripts/${INSTALL_K8S_SCRIPT}"
 
@@ -26,7 +39,7 @@ install_k8s_deps() {
     wget -nc ${INSTALL_K8S_SCRIPT_URL} -P /local/
     chmod +x /local/${INSTALL_K8S_SCRIPT}
     /local/${INSTALL_K8S_SCRIPT}
-    source $HOME/.profile
+    source ${USER_HOME}/.profile
     popd
 }
 
@@ -51,24 +64,24 @@ install_bpftool() {
 install_ssh_keys() {
     pushd .
 
-    if [[ -f "$HOME/.ssh/id_rsa" ]]; then
+    if [[ -f "${USER_HOME}/.ssh/id_rsa" ]]; then
         echo "id_rsa file already exists"
         return
     fi
 
     # Create the user SSH directory, just in case.
-    mkdir $HOME/.ssh && chmod 700 $HOME/.ssh
+    mkdir ${USER_HOME}/.ssh && chmod 700 ${USER_HOME}/.ssh
 
     # Retrieve the server-generated RSA private key.
-    geni-get key > $HOME/.ssh/id_rsa
-    chmod 600 $HOME/.ssh/id_rsa
+    geni-get key > ${USER_HOME}/.ssh/id_rsa
+    chmod 600 ${USER_HOME}/.ssh/id_rsa
 
     # Derive the corresponding public key portion.
-    ssh-keygen -y -f $HOME/.ssh/id_rsa > $HOME/.ssh/id_rsa.pub
+    ssh-keygen -y -f ${USER_HOME}/.ssh/id_rsa > ${USER_HOME}/.ssh/id_rsa.pub
 
     # If you want to permit login authenticated by the auto-generated key,
     # then append the public half to the authorized_keys file:
-    grep -q -f $HOME/.ssh/id_rsa.pub $HOME/.ssh/authorized_keys || cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+    grep -q -f ${USER_HOME}/.ssh/id_rsa.pub ${USER_HOME}/.ssh/authorized_keys || cat ${USER_HOME}/.ssh/id_rsa.pub >> ${USER_HOME}/.ssh/authorized_keys
     popd
 }
 
@@ -84,13 +97,13 @@ $SUDO chown -R smiano:$(id -g) /var/log/polycube
 install_bpftool
 install_ssh_keys
 
-if grep -q "swapoff" "$HOME/.profile"; then
+if grep -q "swapoff" "${USER_HOME}/.profile"; then
     echo "Swap already disabled"
     exit 0
 else
-    echo "sudo swapoff -a" >> $HOME/.profile
+    echo "sudo swapoff -a" >> ${USER_HOME}/.profile
 fi
 
-source $HOME/.profile
+source ${USER_HOME}/.profile
 $SUDO systemctl daemon-reload
 $SUDO systemctl restart kubelet
